259ef66910de86b008532465a53b9b272ebcd6d9
Vivek Parashar <38409647+vivekparasharr@users.noreply.github.com>
2021-04-15 15:35:16 -0400

    x

--- hr_case_study.py
+++ hr_case_study.py
@@ -1,14 +1,12 @@
 
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# A sample dataset
 df = pd.read_csv('Data/HR_Case_Study.csv', encoding = 'ISO-8859-1')
 
-df.info()
+# Another sample dataset
+# Dataset - https://www.kaggle.com/krismurphy01/data-lab
+df2 = pd.read_csv('Data/HR_Engagement_Sat_Sales_UpdatedV4.0.csv')
 
-df[['Name','Tenure in Company','Gender']].\
-    pivot(index='Name', columns='Gender', values='Tenure in Company').plot(kind='hist')
 
-df.Location.unique()
-
-df.groupby('Location').count()[['Name']].plot(kind='bar', args=())
-plt.ylabel('Number of Employees')
